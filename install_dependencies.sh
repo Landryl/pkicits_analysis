42ad44c211bd09fe1dce6f03b6e6acc74bacec7c
Landryl <Landryl@users.noreply.github.com>
2021-01-08 13:38:22 +0100

    Precise commit number of ukano in dependecy

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,6 +2,7 @@
 
 git clone https://github.com/LCBH/UKano.git
 cd UKano
+git checkout 0c2d8c6
 wget https://prosecco.gforge.inria.fr/personal/bblanche/proverif/proverif2.02pl1.tar.gz
 tar -xf proverif2.02pl1.tar.gz
 make PROVERIF=proverif2.02pl1 all
